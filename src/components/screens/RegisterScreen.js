267d6ce00da03705ad0c634bcd8db06d0bde540d
hik8hik <hik8hik@gmail.com>
2022-03-13 20:59:46 +0000

    Register screen done || untested

---
+++ src/components/screens/RegisterScreen.js
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import "./RegisterScreen.css";
+
+const RegisterScreen = ({ history }) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  // register handler
+  const registerHandler = async (e) => {
+    e.preventDefault();
+
+    const config = {
+      header: { "Content-Type": "application/json" },
+    };
+    if (password !== confirmpassword) {
+      setPassword = "";
+      setConfirmPassword = "";
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+
+      return setError("Password do not match");
+    }
+
+    try {
+      const { data } = await axios.post(
+        "/api/auth/register",
+        { username, email, password },
+        config
+      );
+
+      localStorage.setItem("authToken", data.token);
+
+      history.push("/");
+    } catch (error) {
+      setError(error.response.data.error);
+
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
+  };
+
+  return (
+    <div className="register-screen">
+      <form onSubmit={registerHandler} className="register-screen_form">
+        <h3 className="register-screen_title">Sign Up</h3>
+        {error && <span className="error-message">{error}</span>}
+        <div className="form-group">
+          <label htmlFor="name">Username</label>
+          <input
+            id="name"
+            type="text"
+            required
+            placeholder="Enter Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            type="email"
+            required
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            id="password"
+            type="password"
+            required
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="confirmpassword">Password:</label>
+          <input
+            id="confirmpassword"
+            type="password"
+            required
+            placeholder="Confirm Password"
+            value={confirmpassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </div>
+        <Button type="submit" variant="contained">
+          Signup
+        </Button>
+        ;
+        <span className="register-screen_subtext">
+          Already have an account?<Link to="/login">Login</Link>{" "}
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterScreen;
