686b9965a7045b5275900203d926da65f28f844b
hik8hik <hik8hik@gmail.com>
2022-03-13 17:28:55 +0000

    Created Private Route Validator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 const App = () => {
-  return <div className="App">React App</div>;
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route exact path="/login" component={LoginScreen} />
+          <Route exact path="/register" component={RegisterScreen} />
+          <Route exact path="/forgotpassword" component={ForgotPasswordScreen} />
+          <Route exact path="/passwordreset/:resetToken" component={ResetPasswordScreen} />
+        </Switch>
+        </div>
+    </Router>
+  );
 };
 
 export default App;
